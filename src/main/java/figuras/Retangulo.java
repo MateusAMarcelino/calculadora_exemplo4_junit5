9822cf8babda3e5b5b467e7a2322735ed23d3270
Mateus Aloisio Marcelino <MateusA.Marcelino@gmail.com>
2025-04-15 10:59:36 -0300

    Update Retangulo.java

--- src/main/java/figuras/Retangulo.java
+++ src/main/java/figuras/Retangulo.java
@@ -1,35 +1,41 @@
 
-package figuras;
+    package figuras;
 
-/**
- *
- * @author 1072321293
- */
-public class Retangulo extends Figura {
- private double altura;
- private double base;
- public Retangulo() {
- this("", 0, 0);
- }
- public Retangulo(String cor, double base, double altura) {
- super(cor);
- setBase(base);
- setAltura(altura);
- }
- public void setAltura(double altura) {
- this.altura = altura;
- }
- public void setBase(double base) {
- this.base = base;
- }
- public double getAltura() {
- return altura;
- }
- public double getBase() {
- return base;
- }
- @Override
- public double getArea() {
- return (getBase() * getAltura());
- }
-}
+    /**
+     *
+     * @author MateusAMarcelino
+     */
+    public class Retangulo extends Figura {
+     private double altura;
+     private double base;
+     public Retangulo() {
+     this("", 0, 0);
+     }
+
+     public Retangulo(String cor, double base, double altura) {
+     super(cor);
+     setBase(base);
+     setAltura(altura);
+     }
+
+     public void setAltura(double altura) {
+     this.altura = altura;
+     }
+
+     public void setBase(double base) {
+     this.base = base;
+     }
+
+     public double getAltura() {
+     return altura;
+     }
+
+     public double getBase() {
+     return base;
+     }
+
+     @Override
+     public double getArea() {
+     return (getBase() * getAltura());
+     }
+    }
